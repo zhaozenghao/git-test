cb497ef7886112dea1757bf390cb30cf86b5185a
2101class <398399012@qq.com>
2021-08-24 21:34:03 +0800

    第一个测试

---
+++ src/test/java/com/example/gittest/GitTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(1111);
+    }
+
+}
