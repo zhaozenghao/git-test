d34b6db695709f3677284aed310825d264ebfa69
2101class <1919665220@qq.com>
2021-08-24 23:48:10 +0800

    zyc1第二次

---
+++ src/main/java/B.java
@@ -0,0 +1,14 @@
+public class B {
+    private String id;
+    private String name;
+    private String bir;
+
+    public B() {
+    }
+
+    public B(String id, String name, String bir) {
+        this.id = id;
+        this.name = name;
+        this.bir = bir;
+    }
+}
